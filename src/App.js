858215e0328d34980af59a3f233db6e520710f7e
komarnytskiyivan <komar.ivan.97@gmail.com>
2020-12-10 23:02:15 +0200

    Fixed bugs manually

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import HeroSection from './components/HeroSection/HeroSection'
-import SliderSection from './components/SliderSection/SliderSection'
 function App() {
   return (
     <div className="App">
